623e7efcc82838b5ad7de94ec2031553e5ab3abd
nguyenanhky <chienky2016@gmail.com>
2023-10-28 19:06:32 +0700

    start

--- app/src/main/java/kynv1/fsoft/basic/fragments/CounterFragment.kt
+++ app/src/main/java/kynv1/fsoft/basic/fragments/CounterFragment.kt
@@ -6,13 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kynv1.fsoft.basic.R
+import kynv1.fsoft.basic.databinding.CounterFragmentBinding
 
-class CounterFragment: Fragment() {
+class CounterFragment : Fragment() {
+
+    private var _binding: CounterFragmentBinding? = null
+    val binding
+        get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.counter_fragment, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = CounterFragmentBinding.inflate(inflater, container, false);
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
     companion object {
         fun newInstance() = CounterFragment()
--- app/src/main/java/kynv1/fsoft/basic/fragments/ListCounterFragment.kt
+++ app/src/main/java/kynv1/fsoft/basic/fragments/ListCounterFragment.kt
@@ -7,23 +7,38 @@ import android.view.View
 import android.view.ViewGroup
 import kynv1.fsoft.basic.NavigationInterface
 import kynv1.fsoft.basic.R
+import kynv1.fsoft.basic.databinding.CountersFragmentBinding
 
-class ListCounterFragment: Fragment() {
+class ListCounterFragment : Fragment() {
+    private var _binding: CountersFragmentBinding? = null
+    val binding
+        get() = _binding!!
 
     private val navigationController by lazy {
         requireActivity() as? NavigationInterface
     }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.counters_fragment, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = CountersFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        newCounter.setOnClickListener {
-//            navigationController?.navigateTo(CounterFragment.newInstance())
-//        }
+        binding.newCounter.setOnClickListener {
+            navigationController?.navigateTo(CounterFragment.newInstance())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
